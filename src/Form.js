1c142357c1e1a0a9aaee10c107f309222d5076e3
Mohamad Balawi <malek22139@gmail.com>
2022-08-03 20:59:56 +0300

    edit

--- src/Form.js
+++ src/Form.js
@@ -6,25 +6,20 @@ import './Form.css';
 export default function Form() {
   return (
     <form className='Form'>
-      <FormField
-        className='Form-FormField-name'
-        title='Name'
-        type='text'
-        isRequired={true}
-        svg={IconIdentity}
-      />
-      <FormField
-        className='Form-FormField-salary'
-        title='Salary'
-        type='text'
-        isRequired={true}
-      />
-      <FormField
-        className='Form-FormField-age'
-        title='Age'
-        type='text'
-        isRequired={true}
-      />
+      <div className='Form-FormField-name'>
+        <FormField
+          title='Name'
+          type='text'
+          isRequired={true}
+          svg={IconIdentity}
+        />
+      </div>
+      <div className='Form-FormField-salary'>
+        <FormField title='Salary' type='text' isRequired={true} />
+      </div>
+      <div className='Form-FormField-age'>
+        <FormField title='Age' type='text' isRequired={true} />
+      </div>
       <button className='Form-button-submit'>Submit</button>
       <button className='Form-button-update'>Update</button>
     </form>
